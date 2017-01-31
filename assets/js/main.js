215d4ca161b834351641f2a0ccbeeb8281595ae5
Chris Yap <chris.yap@Chris-MacBook-Pro.local>
2017-01-31 17:45:39 +1100

    chart added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,19 @@ $('.chart-trigger').on("click", function(){
   console.log('clicked');
   $(this).find('i').toggleClass('hidden');
   $('.chart').slideToggle('slow');
-})
+});
+
+
+window.odometerOptions = {
+  auto: false, // Don't automatically initialize everything with class 'odometer'
+  selector: '#mins', // Change the selector used to automatically find things to be animated
+  format: '(,ddd)', // Change how digit groups are formatted, and how many digits are shown after the decimal point
+  duration: 3000, // Change how long the javascript expects the CSS animation to take
+  theme: 'default', // Specify the theme (if you have more than one theme css file on the page)
+  animation: 'count' // Count is a simpler animation method which just increments the value,
+                     // use it when you're looking for something more subtle.
+};
+
+setTimeout(function(){
+    mins.innerHTML = 456;
+}, 1000);
